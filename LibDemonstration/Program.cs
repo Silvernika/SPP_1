0e113c7d00d9e457d2e42dc9d7bbd80d3a0070b0
pauliuchenka <pauliuchenka@PAVLYCHENKO-10N>
2024-03-18 21:37:30 +0300

    Добавьте файлы проекта.

---
+++ LibDemonstration/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using LibDemonstration.SomeClasses;
+using Tracer;
+
+
+namespace LibDemonstration
+{
+    class Program
+    {
+        static private Tracer.Tracer _tracer;
+        static private Tracer.Tracer _tracer1;
+        static private Tracer.Tracer _tracer2;
+
+        static void Main()
+        {
+            _tracer1 = new Tracer.Tracer(Thread.CurrentThread.ManagedThreadId);
+
+            Thread thread1 = new Thread(Thread1);
+            Thread thread2 = new Thread(Thread2);
+
+            thread1.Start();
+            thread2.Start();
+
+            Foo foo = new Foo(_tracer1);
+            foo.MyMethod();
+
+            thread1.Join();
+            thread2.Join();
+
+            _tracer2.GetTraceResult();
+            _tracer.GetTraceResult();
+            _tracer1.GetTraceResult();
+
+            _tracer.GetMultiThreadResult("..//..//..//outputJSON.txt", "..//..//..//outputXML.txt");
+        }
+
+        private void SimpleTest()
+        {
+            _tracer = new Tracer.Tracer(Thread.CurrentThread.ManagedThreadId);
+
+            //MethodA
+            //-MethodB
+            //-MethodC
+            //--MethodD
+            MyClass myObject = new MyClass(_tracer);
+            myObject.MethodA();
+
+            _tracer.GetTraceResult();
+
+            _tracer.GetMultiThreadResult("..//..//..//outputJSON.txt", "..//..//..//outputXML.txt");
+            //_tracer.ConsoleResult();
+            //_tracer.FileOutputResult("..//..//..//outputJSON.txt", "..//..//..//outputXML.txt");
+        }
+
+        static public void Thread1()
+        {
+            _tracer = new Tracer.Tracer(Thread.CurrentThread.ManagedThreadId);
+            Foo foo = new Foo(_tracer);
+
+            foo.MyMethod();
+            foo.MySecondMethod();
+        }
+        static public void Thread2()
+        {
+            _tracer2 = new Tracer.Tracer(Thread.CurrentThread.ManagedThreadId);
+            Foo foo = new Foo(_tracer2);
+            Bar bar = new Bar(_tracer2);
+
+            foo.MyMethod();
+            bar.InnerMethod();
+        }
+
+    }
+}
---
+++ LibDemonstration/SomeClasses/MyClassTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer;
+
+namespace LibDemonstration.SomeClasses
+{
+    class MyClass
+    {
+        private ITracer _tracer;
+
+        internal MyClass(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void MethodA()
+        {
+            _tracer.StartTrace();
+
+            // Ваш код метода MethodA
+
+            MethodB();
+            MethodC();
+
+            _tracer.StopTrace();
+        }
+
+        private void MethodB()
+        {
+            _tracer.StartTrace();
+
+            // Ваш код метода MethodB
+            _tracer.StopTrace();
+        }
+
+        private void MethodC()
+        {
+            _tracer.StartTrace();
+
+            // Ваш код метода MethodC
+
+            MethodD();
+            _tracer.StopTrace();
+        }
+
+        private void MethodD()
+        {
+            _tracer.StartTrace();
+
+            // Ваш код метода MethodD
+            _tracer.StopTrace();
+        }
+    }
+}
